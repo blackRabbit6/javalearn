e8d3e653655a1ab9642b4672636a1516476c0f07
blackRabbit6 <1203tak@naver.com>
2023-12-28 22:26:24 +0900

    변수
    변수 초반까지

---
+++ counting.java
@@ -0,0 +1,25 @@
+public class counting {
+
+    public static void main(String[] args) {
+
+//        1) 각각 지정
+//        int x;
+//        int y;
+        
+//        2) 한번에 지정
+        int x, y;
+
+        x = 10;
+        y = 50;
+
+        int plus = x + y;
+        int minus = x- y;
+        int mul = x * y;
+        double div = y / x;
+
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(mul);
+        System.out.println(div);
+    }
+}
